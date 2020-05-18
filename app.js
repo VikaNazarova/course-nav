3f9aa9b5538c51b1421a7378df7c27508e2083d3
VikaNazarova <vikanaz98@gmail.com>
2020-05-18 21:18:14 +0400

    started search module

--- app.js
+++ app.js
@@ -6,12 +6,12 @@
         const xhr = new XMLHttpRequest();
         xhr.open('GET', 'data.json', true);
         xhr.addEventListener('load', () => {
-        const response = JSON.parse(xhr.responseText);
-        callback(response);
+            const response = JSON.parse(xhr.responseText);
+            callback(response);
         });
     
         xhr.addEventListener('error', () => {
-        console.log('Not connected to resource');
+            console.log('Not connected to resource');
         });
     
         xhr.send();
@@ -38,4 +38,38 @@
         loadLocalCourses(showListOfCourses);
     });
 
+//end of module
+
+
+
 // Module of search
+    //MODULE
+    function getSearchResult(arr){
+        
+        let result = arr.find(course => course.name === arr).name
+
+        return result;
+    }
+
+    //VIEW
+    let searchResults = document.querySelector('#searchResults');
+
+    function outputSearchResult (result) {
+        
+        // actual result is probably gonna be deconstructed and showed in a better way lol
+        searchResults.innerHTML = `<p>${result}</p>`;
+    }
+
+    //CONTROLLER
+    // Event of inputting search
+    let searchField = document.querySelector('#searchField');
+
+    searchField.addEventListener('keyup', function(e){
+        let searchQuery = e.target.value;
+        
+        loadLocalCourses(getSearchResult);
+        
+    });
+
+    // here should be also an event of actually submitting search query with pushing search button
+    // and it might or might not lead to another page ??? idk
