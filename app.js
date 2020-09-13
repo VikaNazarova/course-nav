81b8fec2e97c2128cd6263adbc65b548a151351d
VikaNazarova <vikanaz98@gmail.com>
2020-09-14 00:27:05 +0400

    fixed search

--- app.js
+++ app.js
@@ -43,42 +43,64 @@ function ifError(err) {
 
 let searchResults = document.querySelector('#searchResults');
 let searchField = document.querySelector('#searchField');
+// let searchButton = document.querySelector('#searchButton');
+let searchForm = document.querySelector('#searchForm');
 
 function outputSearchResult(result) {
     searchResults.innerHTML = '';
+    searchResults.style.background = '#eee';
     let output = document.createElement('div');
-    output.innerHTML = `<h2>Search results: </h2>`;
-
+    //output.innerHTML = `<h2>Search results: </h2>`;
+    output.classList.add('card-deck');
     if (result.length !== 0) {
-        for (var {course_title, image, description, link} of result) {
+        for (var {course_title, image, description, link, provider, price} of result) {
+            var shortDesc = description.substr(0, 200) + "...";
             output.innerHTML += `
-            <div>
-                <h3>${course_title}</h3>
+            <div class="card">
                 <img src="${image}">
-                <p>${description}</p>
+                <h3>${course_title}</h3>
+                <div>
+                <small>by ${provider}, </small>
+                <small>${price}</small>
+                </div>
+                <p>${shortDesc}</p>
                 <a href="${link}">Go to course</a>
             <div>`;
         }
-
     } else {
-        output.innerHTML += `<p>No results</p>`;
+        output.innerHTML += `<p>No results, try something different</p>`;
     }
-
-    output.innerHTML += `<br><hr>`;
+    output.innerHTML += `<br>`;
+    searchResults.innerHTML = '<h2>Search results: </h2>';
     searchResults.appendChild(output);
 }
 
-searchField.addEventListener('keyup', function(e){ // here should be probably also an event of actually submitting search query with pushing search button
-    let searchQuery = e.target.value;
 
-    fetch('https://gist.githubusercontent.com/VikaNazarova/18108021850f79e70fbdc46a62056293/raw/45bb37abc84868f2c78bc83de541438700cdcbc9/gistfile1.txt')
-    .then(response => response.json())
-    .then(courses => 
-        outputSearchResult(
-            courses.filter(course => course.course_title.toLowerCase().includes(searchQuery.toLowerCase()))
-        )
-    ) 
-    .catch(err => ifError(err));
+searchForm.addEventListener('submit', function(e){
+        e.preventDefault();
+        let searchQuery = searchField.value;
+
+        fetch('https://gist.githubusercontent.com/VikaNazarova/18108021850f79e70fbdc46a62056293/raw/45bb37abc84868f2c78bc83de541438700cdcbc9/gistfile1.txt')
+        .then(response => response.json())
+        .then(courses => 
+            outputSearchResult(
+                courses.filter(course => course.course_title.toLowerCase().includes(searchQuery.toLowerCase()))
+            )
+        ) 
+        .catch(err => ifError(err));
 });
+
+// searchField.addEventListener('keyup', function(e){
+//     let searchQuery = e.target.value;
+
+//     fetch('https://gist.githubusercontent.com/VikaNazarova/18108021850f79e70fbdc46a62056293/raw/45bb37abc84868f2c78bc83de541438700cdcbc9/gistfile1.txt')
+//     .then(response => response.json())
+//     .then(courses => 
+//         outputSearchResult(
+//             courses.filter(course => course.course_title.toLowerCase().includes(searchQuery.toLowerCase()))
+//         )
+//     ) 
+//     .catch(err => ifError(err));
+// });
 
 //end of module
