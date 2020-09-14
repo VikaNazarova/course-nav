02a003a17386489fe99a8fac8e47ea8e6dcfc969
VikaNazarova <vikanaz98@gmail.com>
2020-09-14 12:37:27 +0400

    fixed fetch's link

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Module of getting courses (External API)
 
-fetch('https://gist.githubusercontent.com/VikaNazarova/18108021850f79e70fbdc46a62056293/raw/45bb37abc84868f2c78bc83de541438700cdcbc9/gistfile1.txt')
+fetch('https://gist.githubusercontent.com/VikaNazarova/18108021850f79e70fbdc46a62056293/raw/2f05dceca54ea0332417525fba6a700b49434c27/gistfile1.txt')
 .then(response => response.json())
 .then(courses => showListOfCourses(courses))
 .catch(err => ifError(err));
@@ -18,7 +18,9 @@ function showListOfCourses(arr) {
       output.classList.add('card');
       var shortDesc = description.substr(0, 200) + "...";
       output.innerHTML = `
-      <img src="${image}">
+      <a href="${link}">
+        <img src="${image}">
+      </a>
       <h3>${course_title}</h3>
       <div>
         <small>by ${provider}, </small>
@@ -80,7 +82,7 @@ searchForm.addEventListener('submit', function(e){
         e.preventDefault();
         let searchQuery = searchField.value;
 
-        fetch('https://gist.githubusercontent.com/VikaNazarova/18108021850f79e70fbdc46a62056293/raw/45bb37abc84868f2c78bc83de541438700cdcbc9/gistfile1.txt')
+        fetch('https://gist.githubusercontent.com/VikaNazarova/18108021850f79e70fbdc46a62056293/raw/2f05dceca54ea0332417525fba6a700b49434c27/gistfile1.txt')
         .then(response => response.json())
         .then(courses => 
             outputSearchResult(
